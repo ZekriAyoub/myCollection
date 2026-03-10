85e79a3cc157e2b2371b4c57592d854898c245a0
ZekriAyoub <ayoub.zekri.3@icloud.com>
2026-03-10 21:44:05 +0100

    add InputSignals

--- collection-manager/src/app/app.ts
+++ collection-manager/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CollectionItemCard } from './components/collection-item-card/collection-item-card';
+import { CollectionItem } from './models/collection-items';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,14 @@ import { CollectionItemCard } from './components/collection-item-card/collection
 
 })
 export class App {
-  protected readonly title = signal('collection-manager');
+  coin! : CollectionItem;
+
+  constructor() {
+    this.coin = new CollectionItem();
+    this.coin.name = "Piece de 1980";
+    this.coin.description = "Une pièce de monnaie rare datant de 1980, en excellent état.";
+    this.coin.rarity = "Commune";
+    this.coin.price = 170;
+    this.coin.image = "img/coin1.png";
+  }
 }
