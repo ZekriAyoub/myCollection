f9b96723df13f04e44703a5be09dac26fce2d31a
ZekriAyoub <ayoub.zekri.3@icloud.com>
2026-03-14 15:51:00 +0100

    add two-way binding for search field

--- collection-manager/src/app/components/search-bar/search-bar.ts
+++ collection-manager/src/app/components/search-bar/search-bar.ts
@@ -10,14 +10,14 @@ import { FormsModule } from '@angular/forms';
 export class SearchBar {
 
   search = input("Initial");
-  searchTextChange = output<string>();
+  searchChange = output<string>();
 
-  searchButtonClicked: OutputEmitterRef<void> = output<void>();
+  searchButtonClicked: OutputEmitterRef<void> = output<void>({});
   searchClicked(){
     this.searchButtonClicked.emit();
   }
 
   updateSearch(searchText: string){
-    this.searchTextChange.emit(searchText);
+    this.searchChange.emit(searchText);
   }
 }
