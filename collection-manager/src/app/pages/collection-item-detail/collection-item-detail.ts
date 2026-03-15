2930ddc01b0264e1f4f4b292188997f8c5467b98
ZekriAyoub <ayoub.zekri.3@icloud.com>
2026-03-15 17:25:59 +0100

    add withComponentInputBinding to simplify the collect of id in url

--- collection-manager/src/app/pages/collection-item-detail/collection-item-detail.ts
+++ collection-manager/src/app/pages/collection-item-detail/collection-item-detail.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { Component, inject, input} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-collection-item-detail',
@@ -8,34 +7,20 @@ import { Subscription } from 'rxjs';
   templateUrl: './collection-item-detail.html',
   styleUrl: './collection-item-detail.css',
 })
-export class CollectionItemDetail implements OnInit, OnDestroy{
+export class CollectionItemDetail {
 
   private readonly activedRoute = inject(ActivatedRoute);
   private readonly router = inject(Router);
 
-  itemId = signal<number | null>(null);
-
-  routeSubscription : Subscription | null = null;
-
-  ngOnInit(): void {
-    this.routeSubscription = this.activedRoute.params.subscribe(
-      (params) => {
-        const selectedId = params['id'] ? parseInt(params['id']) : null;
-        this.itemId.set(selectedId);
-      }
-    );
-  }
-
-  ngOnDestroy(): void {
-    if (this.routeSubscription){
-      this.routeSubscription.unsubscribe();
-    }
-  }
+  itemId = input<number | null, string | null>(null, {
+    alias: 'id',
+    transform: value => value ? parseInt(value) : null
+  });
 
   next() {
-    const currentd = this.itemId();
-    if(currentd){
-      const nextId = currentd + 1;
+    const currentId = this.itemId();
+    if(currentId){
+      const nextId = currentId + 1;
       this.router.navigate(['item', nextId])
     }
   }
