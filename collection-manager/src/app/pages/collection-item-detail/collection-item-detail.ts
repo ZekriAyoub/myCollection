d06b1d3600d5b61c40d05c0d758159b09036d013
ZekriAyoub <ayoub.zekri.3@icloud.com>
2026-03-15 17:06:07 +0100

    add onInit and get params in url

--- collection-manager/src/app/pages/collection-item-detail/collection-item-detail.ts
+++ collection-manager/src/app/pages/collection-item-detail/collection-item-detail.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-collection-item-detail',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './collection-item-detail.html',
   styleUrl: './collection-item-detail.css',
 })
-export class CollectionItemDetail {}
+export class CollectionItemDetail implements OnInit {
+
+  private readonly activedRoute = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  itemId = signal<number | null>(null);
+
+  ngOnInit(): void {
+    const params = this.activedRoute.snapshot.params;
+    const selectedId = params['id'] ? parseInt(params['id']) : null;
+    this.itemId.set(selectedId);
+  }
+
+  next() {
+    const currentd = this.itemId();
+    if(currentd){
+      const nextId = currentd + 1;
+      this.router.navigate(['item', nextId])
+    }
+  }
+
+
+}
